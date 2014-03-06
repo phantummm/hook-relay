70e5c7a7710689ef92542f51ef005acdce85fcf1
phantummm <phantummm@gmail.com>
2014-03-05 20:10:00 -0500

    Fixes div headers; kicks .sass-cache out of the repo

--- db/migrate/20140305052231_create_destinations.rb
+++ db/migrate/20140305052231_create_destinations.rb
@@ -2,7 +2,7 @@ class CreateDestinations < ActiveRecord::Migration
   def change
     create_table :destinations do |t|
       t.string :title
-      t.string :url, :unique => true, :null => false
+      t.string :url, :null => false
       t.string :extra_parameters
       t.timestamps
     end
