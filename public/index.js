80b594dbaabf93c1d9d1cbdbb41b32693aced5bb
phantummm <phantummm@gmail.com>
2014-03-05 23:43:30 -0500

    Finished up front-end

--- public/index.js
+++ public/index.js
@@ -1,19 +1,33 @@
+var initialize = function($scope) {
+  getSources($scope);
+  bindAddKeys($scope);
+}
+
 var refreshDestinations = function($scope, source) {
   var strang;
 
+  $('.menu-item.destination').remove();
+  $('.destination-title span').remove();
+
+  $('.destination-title h3').append("<span>for app [<span class='app-title'>" +
+                             source.source.title + "</span>]</span>");
+
   $.each(source.destinations, function(key, value) {
     strang = value.title + " (" + value.url + ")";
-    $('.destination-inner').append("<div class='menu-item'>" + strang + "</div>");
+    $('.destination-inner').append("<div class='menu-item destination'>" + strang + "</div>");
   });
+  $('.plus-icon.add-destination-icon').show();
 }
 
 var refreshSources = function($scope) {
+  var strang;
+
+  $('.menu-item.source').remove();
 
   $.each($scope.sources, function(key, value) {
     strang = value.source.title + " (" + value.source.url + ")";
-    $('.source-inner').append("<div class='menu-item'>" + strang + "</div>");
+    $('.source-inner').append("<div class='menu-item source' data-index='" + key + "'>" + strang + "</div>");
   });
-  refreshDestinations($scope, $scope.sources[1]);
 }
 
 var getSources = function($scope) {
@@ -23,11 +37,29 @@ var getSources = function($scope) {
     success: function(data) {
       $scope.sources = data;
       refreshSources($scope);
+      bindSourceKeys($scope);
     }
+  });
+}
+
+var bindSourceKeys = function($scope) {
+  $('.menu-item.source').click(function() {
+    var index = $(this).data('index');
+
+    $('.menu-item.source').removeClass('selected');
+    $(this).addClass('selected');
+    refreshDestinations($scope, $scope.sources[index]);
+  });
+}
+
+var bindAddKeys = function($scope) {
+  $('.plus-icon').click(function() {
+    $(this).toggleClass('x-icon');
+    $(this).siblings('.add-form').toggle();
   });
 }
 
 $(window).ready(function() {
   $scope = {};
-  getSources($scope);
+  initialize($scope);
 });
